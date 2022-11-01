c471a5d85db490d5783f89a03211b1ce209a2af1
Jonathan Gray <zeusdcs@gmail.com>
2022-11-01 10:06:32 -0400

    Initial commit of Lucidity Chat App - socket.io

---
+++ index.js
@@ -0,0 +1,102 @@
+const 
+    Message = require("./models/Message"),
+    mongoose = require("mongoose"),
+    path = require("path")
+    cors = require("cors"),
+    express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    { Server } = require("socket.io"),
+    io = new Server(server);
+    PORT = process.env.PORT || 5000;
+
+/**
+ * ================================= Mongoose ======================================== /
+ */
+
+mongoose.connect(
+    `mongodb+srv://jonG:GosoJbcx2jxdojXW@application.h00aiy3.mongodb.net/Data`,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+)
+.then(
+    console.log("\n\tConnected to mongoDB")
+)
+.catch(error => console.log("Error: ", error))
+
+/* ------------------------------ */
+
+// Listen for changes to Messages model in DB
+
+let tempNewMessageObj = undefined
+const messageEventEmitter = Message.watch()
+
+messageEventEmitter.on("change", (change => {
+    tempNewMessageObj = change.fullDocument
+    // console.log(tempNewMessageObj)
+}))
+
+
+/**
+ * ================================= Express ======================================== /
+ */
+
+app.use(
+    cors({
+        origin: "*"
+    })
+);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", (req, res) => {
+    res.render("index.html")
+})
+
+io.on("connection", (socket) => {
+
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected")
+    })
+
+    socket.on("message", (message) => {
+        socket.broadcast.emit("message", message)
+    })
+
+})
+
+app.get("/loadmessages", (req, res) => {
+    Message.find({})
+    .then(data => res.json(data))
+})
+
+app.get("/newmessage", (req, res) => {
+    let data = tempNewMessageObj
+    res.json(data)
+})
+
+app.get("/test", (req, res) => {
+    res.json({test: "this is a json test string"})
+})
+
+app.post("/addmessage", (req, res) => {
+
+    const newMessage = new Message({
+        message: req.body.message,
+    })
+
+    newMessage.save().then(() => console.log("\nSuccessfully saved message..."))
+
+    res.sendStatus(200)
+})
+
+server.listen(PORT, () => console.log(`\n\tListening: http://localhost:${PORT}\n`))
+
---
+++ public/js/script.js
@@ -0,0 +1,73 @@
+var socket = io();
+const test = document.querySelector(".test")
+const testMessage = document.querySelector(".testMessage")
+
+/*--------------------------------------------------*/
+
+// Load messages from DB to the client
+
+    // https://server-lucidity-chat-app.vercel.app
+
+fetch("/loadmessages")
+    .then(data => data.json())
+    .then(data => data.forEach(messageObj => {
+        appendMessage(messageObj.message)
+    }))
+
+/*--------------------------------------------------*/
+
+// Listen for incoming messages
+
+socket.on("message", (message) => {
+    appendMessage(message)
+})
+
+/*--------------------------------------------------*/
+
+// Submit new message to Server
+
+const form = document.querySelector(".form")
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    let message = event.target.message.value;
+
+    socket.emit("message", message)
+    appendMessage(message);
+    addToDB(message);
+    event.target.message.value = "";
+
+})
+
+/*--------------------------------------------------*/
+
+// Add message to DB 
+
+function addToDB(message) {
+    fetch("/addmessage", {
+        method: "POST",
+
+        body: JSON.stringify({
+            message: message
+        }),
+
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .catch(e => {console.log("Errors in saving message in DB: ", e)})
+}
+
+/*--------------------------------------------------*/
+
+// Function appends messages to document
+
+function appendMessage(message) {
+    const newDiv = document.createElement("div")
+    
+    newDiv.classList.add("testMessage")
+    newDiv.textContent = message
+    test.appendChild(newDiv)
+    test.scrollTo(0, test.scrollHeight);
+}
